0f4c441358adb7c816b7b5f8a78b9793572b78c1
Jiri Hoffmann <hoffmaji@mail.gvsu.edu>
2021-11-27 22:16:29 -0500

    Working Android build with JSI

--- example/android/app/src/main/java/com/example/reactnativeclusterer/MainApplication.java
+++ example/android/app/src/main/java/com/example/reactnativeclusterer/MainApplication.java
@@ -7,10 +7,12 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.bridge.JSIModulePackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.reactnativeclusterer.ClustererPackage;
+
+import com.reactnativeclusterer.ClustererModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,13 +29,18 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for ClustererExample:
           // packages.add(new MyReactNativePackage());
-          packages.add(new ClustererPackage());
+//          packages.add(new ClustererPackage());
           return packages;
         }
 
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ClustererModulePackage();
         }
       };
 
