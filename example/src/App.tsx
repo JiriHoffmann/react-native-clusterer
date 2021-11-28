5ab9da13b0876fc38a66ea14e08a052833393553
Jiri Hoffmann <hoffmaji@mail.gvsu.edu>
2021-11-27 22:35:44 -0500

    Working Android multiply example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,31 @@
-import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import { multiply } from 'react-native-clusterer';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const [result, setResult] = useState<number | undefined>();
+
+  const _handlePress = () => {
+    setResult(multiply(parseInt(input1), parseInt(input2)));
+  };
 
   return (
     <View style={styles.container}>
+      <TextInput
+        value={input1}
+        onChangeText={setInput1}
+        style={styles.textInput}
+      ></TextInput>
+      <TextInput
+        value={input2}
+        onChangeText={setInput2}
+        style={styles.textInput}
+      ></TextInput>
+      <Button title={'Get Result'} onPress={_handlePress} />
       <Text>Result: {result}</Text>
     </View>
   );
@@ -27,5 +41,12 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  textInput: {
+    borderColor: 'black',
+    borderWidth: 1,
+    height: 40,
+    width: 200,
+    margin: 10,
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,6 @@
-import { NativeModules, Platform } from 'react-native';
-
-const LINKING_ERROR =
-  `The package 'react-native-clusterer' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo managed workflow\n';
-
-const Clusterer = NativeModules.Clusterer
-  ? NativeModules.Clusterer
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-export function multiply(a: number, b: number): Promise<number> {
-  return Clusterer.multiply(a, b);
+function multiply(a: number, b: number): number {
+  // @ts-ignore
+  return global.multiply(a, b);
 }
+
+export { multiply };
