23395120d39e1fbfc852c81603883e81d6a39aac
Jiri Hoffmann <hoffmaji@mail.gvsu.edu>
2021-12-09 15:14:21 -0500

    Examples cleanup

--- example/src/superclustercpp.tsx
+++ example/src/superclustercpp.tsx
@@ -2,46 +2,34 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button, ScrollView } from 'react-native';
 import { PerformanceNow, superclusterOptions, timeDelta } from './utils';
 import { getRandomData } from './places';
-
-const initialRegion = {
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421
-}
+import Clusterer from 'react-native-clusterer';
 
 export default function SuperclusterCPP() {
-    const [region, setRegion] = useState(initialRegion);
-    const [time, setTime] = useState({ init: 0, getTile: 0, getClusters: 0 });
-    const [points, setPoints] = useState([]);
+    const [time, setTime] = useState<string[]>(["0", "0"]);
+    const [points, setPoints] = useState<string>("");
 
     const _handleRenderPress = () => {
         const initS = PerformanceNow();
-        clustererModule.init("test", superclusterOptions, getRandomData(10000));
+        const supercluster = new Clusterer(getRandomData(10000), superclusterOptions);
         const initE = PerformanceNow();
 
         const getTileS = PerformanceNow();
-        const tileRes = clustererModule.getTile("test", 0, 0, 0);
+        const tileRes = supercluster.getTile(0, 0, 0);
         const getTileE = PerformanceNow();
 
-        setPoints(tileRes);
-        setTime({
-            init: timeDelta(initS, initE),
-            getTile: timeDelta(getTileS, getTileE),
-            getClusters: 0
-        })
+        setPoints(JSON.stringify(tileRes));
+        setTime([timeDelta(initS, initE),timeDelta(getTileS, getTileE)])
     }
 
 
     return (
         <View style={styles.container}>
-            <Text style={styles.header}>Supercluster CPP library</Text>
-            <Text  style={styles.h2}>Initialization time: {time.init} ms</Text>
-            <Text style={styles.h2}>Get tile time: {time.getTile} ms</Text>
+            <Text style={styles.header}>Supercluster C++ library</Text>
+            <Text style={styles.h2}>Iitialization time: {time[0]} ms</Text>
+            <Text style={styles.h2}>Get tile time: {time[1]} ms</Text>
             <Text style={styles.h2}>Tile result:</Text>
-            <ScrollView style={styles.scrollView}> 
-
-            <Text>{JSON.stringify(points)}</Text>
+            <ScrollView style={styles.scrollView}>
+                <Text>{points}</Text>
             </ScrollView>
             <Button title={'Render'} onPress={_handleRenderPress} />
 
@@ -54,18 +42,18 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingBottom: 50
     },
-    scrollView:{
+    scrollView: {
         marginBottom: 10
     },
     header: {
         fontSize: 20,
         fontWeight: 'bold',
     },
-    h2:{
+    h2: {
         fontSize: 14,
         fontWeight: 'bold',
     }
-    
+
 
 });
 
--- example/src/superclusterjs.tsx
+++ example/src/superclusterjs.tsx
@@ -1,22 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, ScrollView } from 'react-native';
-import { multiply } from 'react-native-clusterer';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button, ScrollView } from 'react-native';
 import Supercluster from 'supercluster';
-import MapView from "react-native-maps"
 import { PerformanceNow, superclusterOptions, timeDelta } from './utils';
 import { getRandomData } from './places';
 
-const initialRegion = {
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421
-}
-
 export default function SuperclusterJS() {
-    const [region, setRegion] = useState(initialRegion);
-    const [time, setTime] = useState({ init: 0, getTile: 0, getClusters: 0 });
-    const [points, setPoints] = useState([]);
+    const [time, setTime] = useState<string[]>(["0", "0"]);
+    const [points, setPoints] = useState<string>("");
 
     const _handleRenderPress = () => {
         const initS = PerformanceNow();
@@ -28,24 +18,19 @@ export default function SuperclusterJS() {
         const tileRes = supercluster.getTile(0, 0, 0);
         const getTileE = PerformanceNow();
 
-        setPoints(tileRes);
-        setTime({
-            init: timeDelta(initS, initE),
-            getTile: timeDelta(getTileS, getTileE),
-            getClusters: 0
-        })
+        setPoints(JSON.stringify(tileRes));
+        setTime([timeDelta(initS, initE),timeDelta(getTileS, getTileE)])
     }
 
 
     return (
         <View style={styles.container}>
             <Text style={styles.header}>Supercluster JS library</Text>
-            <Text  style={styles.h2}>Initialization time: {time.init} ms</Text>
-            <Text style={styles.h2}>Get tile time: {time.getTile} ms</Text>
+            <Text style={styles.h2}>Iitialization time: {time[0]} ms</Text>
+            <Text style={styles.h2}>Get tile time: {time[1]} ms</Text>
             <Text style={styles.h2}>Tile result:</Text>
-            <ScrollView style={styles.scrollView}> 
-
-            <Text>{JSON.stringify(points)}</Text>
+            <ScrollView style={styles.scrollView}>
+                <Text>{points}</Text>
             </ScrollView>
             <Button title={'Render'} onPress={_handleRenderPress} />
 
@@ -58,18 +43,18 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingBottom: 50
     },
-    scrollView:{
+    scrollView: {
         marginBottom: 10
     },
     header: {
         fontSize: 20,
         fontWeight: 'bold',
     },
-    h2:{
+    h2: {
         fontSize: 14,
         fontWeight: 'bold',
     }
-    
+
 
 });
 
