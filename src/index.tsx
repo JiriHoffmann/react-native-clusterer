3f9dc1aca148bb6ce4c47c4d9b5b500d984c3e23
Jiri Hoffmann <hoffmaji@mail.gvsu.edu>
2021-12-09 13:59:02 -0500

    Create Clusterer class

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,43 @@
-function multiply(a: number, b: number): number {
-  // @ts-ignore
-  return global.multiply(a, b);
+//@ts-ignore
+const module = global.clustererModule
+
+const getRandomId = () =>{
+  return `${Math.floor(Math.random() * Math.floor(Math.random() * Date.now()))}`
 }
 
-export { multiply };
+interface SuperclusterOptions {
+  radius?: number,
+  maxZoom?: number,
+  minZoom?: number,
+  extent?: number,
+  minSize?: number,
+}
+
+export default class Clusterer {
+  private id: string
+
+  constructor(points: any, options?: SuperclusterOptions) {
+    this.id = getRandomId()
+    module.init(this.id, points, options);
+  }
+
+  getClusters(bbox:any, zoom: number): any {
+    return null;
+  }
+
+  getTile(x: number, y: number, z: number): any {
+    return module.getTile(this.id, x, y, z);
+  }
+  
+  getChildren(clusterId: string): any {
+    return module.getChildren(this.id, clusterId);
+  }
+
+  getLeaves(clusterId: string, limit?: number, offset?: number): any {
+    return module.getLeaves(this.id, clusterId, limit ?? 10, offset ?? 0);
+  }
+
+  getClusterExpansionZoom(clusterId: string): number {
+    return module.getClusterExpansionZoom(this.id, clusterId);
+  }
+}
