fe73e5f2ea93f1e3d8ce8caec568c4c14642282c
Jiri Hoffmann <hoffmaji@mail.gvsu.edu>
2022-01-30 13:28:45 -0500

    fix(js): types

---
+++ src/index.ts
@@ -0,0 +1,107 @@
+import { NativeModules, Platform } from 'react-native';
+import type Supercluster from './types';
+
+const module = NativeModules.Clusterer;
+
+if (module && typeof module.install === 'function') {
+  module.install();
+}
+
+const clusterer = (global as any).clustererModule;
+
+export default class SuperclusterClass<
+  P extends GeoJSON.GeoJsonProperties = Supercluster.AnyProps,
+  C extends GeoJSON.GeoJsonProperties = Supercluster.AnyProps
+> {
+  private id: string;
+
+  constructor(
+    points: Array<Supercluster.PointFeature<P>>,
+    options?: Supercluster.Options<P, C>
+  ) {
+    if (!clusterer) {
+      throw new Error(
+        `The package 'react-native-cryptopp' doesn't seem to be linked. Make sure: \n\n` +
+          Platform.select({
+            ios: "- You have run 'pod install'\n",
+            default: '',
+          }) +
+          '- You rebuilt the app after installing the package\n' +
+          '- You are not using Expo managed workflow\n'
+      );
+    }
+
+    // generate random id
+    this.id = `${Math.floor(
+      Math.random() * Math.floor(Math.random() * Date.now())
+    )}`;
+    clusterer.init(this.id, points, options);
+  }
+
+  /**
+   * Returns an array of clusters and points as `GeoJSON.Feature` objects
+   * for the given bounding box (`bbox`) and zoom level (`zoom`).
+   *
+   * @param bbox Bounding box (`[westLng, southLat, eastLng, northLat]`).
+   * @param zoom Zoom level.
+   */
+  getClusters(
+    bbox: GeoJSON.BBox,
+    zoom: number
+  ): Array<Supercluster.ClusterFeature<C> | Supercluster.PointFeature<P>> {
+    return clusterer.getClusters(this.id, bbox, zoom);
+  }
+
+  /**
+   * For a given zoom and x/y coordinates, returns a
+   * [geojson-vt](https://github.com/mapbox/geojson-vt)-compatible JSON
+   * tile object with cluster any point features.
+   */
+  getTile(x: number, y: number, zoom: number): Supercluster.Tile<C, P> | null {
+    return { features: clusterer.getTile(this.id, x, y, zoom) };
+  }
+
+  /**
+   * Returns the children of a cluster (on the next zoom level).
+   *
+   * @param clusterId Cluster ID (`cluster_id` value from feature properties).
+   * @throws {Error} If `clusterId` does not exist.
+   */
+  getChildren(
+    clusterId: number
+  ): Array<Supercluster.ClusterFeature<C> | Supercluster.PointFeature<P>> {
+    return clusterer.getChildren(this.id, clusterId);
+  }
+
+  /**
+   * Returns all the points of a cluster (with pagination support).
+   *
+   * @param clusterId Cluster ID (`cluster_id` value from feature properties).
+   * @param limit The number of points to return (set to `Infinity` for all points).
+   * @param offset The amount of points to skip (for pagination).
+   */
+  getLeaves(
+    clusterId: number,
+    limit?: number,
+    offset?: number
+  ): Array<Supercluster.PointFeature<P>> {
+    return clusterer.getLeaves(this.id, clusterId, limit ?? 10, offset ?? 0);
+  }
+
+  /**
+   * Returns the zoom level on which the cluster expands into several
+   * children (useful for "click to zoom" feature).
+   *
+   * @param clusterId Cluster ID (`cluster_id` value from feature properties).
+   */
+  getClusterExpansionZoom(clusterId: number): number {
+    return clusterer.getClusterExpansionZoom(this.id, clusterId);
+  }
+
+  /**
+   * Destroy the instance.
+   */
+  destroy(): void {
+    clusterer.destroyCluster(this.id);
+  }
+}
--- src/types.ts
+++ src/types.ts
@@ -16,56 +16,48 @@ declare namespace Supercluster {
      * @default 0
      */
     minZoom?: number | undefined;
-
     /**
      * Maximum zoom level at which clusters are generated.
      *
      * @default 16
      */
     maxZoom?: number | undefined;
-
     /**
      * Minimum number of points to form a cluster.
      *
      * @default 2
      */
     minPoints?: number | undefined;
-
     /**
      * Cluster radius, in pixels.
      *
      * @default 40
      */
     radius?: number | undefined;
-
     /**
      * (Tiles) Tile extent. Radius is calculated relative to this value.
      *
      * @default 512
      */
     extent?: number | undefined;
-
     /**
      * Whether to generate ids for input features in vector tiles.
      *
      * @default false
      */
     generateId?: boolean | undefined;
-
     /**
      * Size of the KD-tree leaf node. Affects performance.
      *
      * @default 64
      */
     // nodeSize?: number | undefined;
-
     /**
      * Whether timing info should be logged.
      *
      * @default false
      */
     // log?: boolean | undefined;
-
     /**
      * A function that returns cluster properties corresponding to a single point.
      *
@@ -73,7 +65,6 @@ declare namespace Supercluster {
      * (props) => ({sum: props.myValue})
      */
     // map?: ((props: P) => C) | undefined;
-
     /**
      * A reduce function that merges properties of two clusters into one.
      *
@@ -82,7 +73,6 @@ declare namespace Supercluster {
      */
     // reduce?: ((accumulated: C, props: Readonly<C>) => void) | undefined;
   }
-
   /**
    * Default properties type, allowing any properties.
    * Try to avoid this for better typesafety by using proper types.
@@ -90,14 +80,12 @@ declare namespace Supercluster {
   interface AnyProps {
     [name: string]: any;
   }
-
   /**
    * [GeoJSON Feature](https://tools.ietf.org/html/rfc7946#section-3.2),
    * with the geometry being a
    * [GeoJSON Point](https://tools.ietf.org/html/rfc7946#section-3.1.2).
    */
   type PointFeature<P> = GeoJSON.Feature<GeoJSON.Point, P>;
-
   interface ClusterProperties {
     /**
      * Always `true` to indicate that the Feature is a Cluster and not
@@ -114,90 +102,15 @@ declare namespace Supercluster {
      */
     point_count_abbreviated: string;
   }
-
   type ClusterFeature<C> = PointFeature<ClusterProperties & C>;
-
   interface TileFeature<C, P> {
     type: 1;
     geometry: Array<[number, number]>;
     tags: (ClusterProperties & C) | P;
   }
-
   interface Tile<C, P> {
     features: Array<TileFeature<C, P>>;
   }
 }
 
-/**
- * React Native JSI port of a very fast geospatial point clustering library for browsers and Node.
- *
- * @param points Array of GeoJSON Features, the geometries being GeoJSON Points.
- *
- * @param options Optional object containing parameters.
- */
-export default class Supercluster<
-  P extends GeoJSON.GeoJsonProperties = Supercluster.AnyProps,
-  C extends GeoJSON.GeoJsonProperties = Supercluster.AnyProps
-> {
-  private id: number;
-
-  constructor(
-    points: Array<Supercluster.PointFeature<P>>,
-    options?: Supercluster.Options<P, C>
-  );
-
-  /**
-   * Returns an array of clusters and points as `GeoJSON.Feature` objects
-   * for the given bounding box (`bbox`) and zoom level (`zoom`).
-   *
-   * @param bbox Bounding box (`[westLng, southLat, eastLng, northLat]`).
-   * @param zoom Zoom level.
-   */
-  getClusters(
-    bbox: GeoJSON.BBox,
-    zoom: number
-  ): Array<Supercluster.ClusterFeature<C> | Supercluster.PointFeature<P>>;
-
-  /**
-   * For a given zoom and x/y coordinates, returns a
-   * [geojson-vt](https://github.com/mapbox/geojson-vt)-compatible JSON
-   * tile object with cluster any point features.
-   */
-  getTile(zoom: number, x: number, y: number): Supercluster.Tile<C, P> | null;
-
-  /**
-   * Returns the children of a cluster (on the next zoom level).
-   *
-   * @param clusterId Cluster ID (`cluster_id` value from feature properties).
-   * @throws {Error} If `clusterId` does not exist.
-   */
-  getChildren(
-    clusterId: number
-  ): Array<Supercluster.ClusterFeature<C> | Supercluster.PointFeature<P>>;
-
-  /**
-   * Returns all the points of a cluster (with pagination support).
-   *
-   * @param clusterId Cluster ID (`cluster_id` value from feature properties).
-   * @param limit The number of points to return (set to `Infinity` for all points).
-   * @param offset The amount of points to skip (for pagination).
-   */
-  getLeaves(
-    clusterId: number,
-    limit?: number,
-    offset?: number
-  ): Array<Supercluster.PointFeature<P>>;
-
-  /**
-   * Returns the zoom level on which the cluster expands into several
-   * children (useful for "click to zoom" feature).
-   *
-   * @param clusterId Cluster ID (`cluster_id` value from feature properties).
-   */
-  getClusterExpansionZoom(clusterId: number): number;
-
-  /**
-   * Destroy the instance.
-   */
-  destroy();
-}
+export default Supercluster
